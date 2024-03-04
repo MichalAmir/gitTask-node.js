de6527f266cbc1c2119b448af0211367bad3fed4
AvigailH <avigail054855@gmail.com>
2024-03-05 01:23:28 +0200

    The assignment is done

--- routing.js
+++ routing.js
@@ -1,14 +1,11 @@
-const express= require("express")
+const express = require("express");
+const router = express.Router();
+const userController = require('./usercontroller');
 
-const router =express.Router()
+router.get('/users', userController.getAllUsers);
+router.get('/users/:id', userController.getById);
+router.put('/users', userController.putUser);
+router.post('/users/:newName', userController.postUser);
+router.delete('/users/:id', userController.deleteUser);
 
-const userController=require('./usercontroller')
-
-router.get('/users', userController.gatAllUsers)
-router.get('/users/:id', userController.getById)
-router.put('/users/:id', userController.putUser)
-router.post('/users', userController.postUser)
-router.delete('/users/:id', userController.deleteUser)
-
-
-module.exports=router
+module.exports = router;
--- usercontroller.js
+++ usercontroller.js
@@ -1,20 +1,24 @@
 const express = require("express")
-const app = express()
-const port = 8080
-const userController= require('./user_server')
- 
-const users=[{ id:"1",name:"Avigail",email:"Avigail@gmail.com",phone:"0548559877"},
-{ id:"2",name:"shulamit",email:"shulamit@gmail.com",phone:"0587658877"},
-{ id:"3",name:"Michal",email:"Michal@gmail.com",phone:"054987877"},]
-
-app.get('/users',userController.getAllUsers)
-
-app.get('/users/:id',userController.getById)
-
-app.put('/users',userController.putUser)
-
-app.post('/users/:newName',userController.postUser)
-
-app.delete('/users/:id',userController.deleteUser)
-
- 
+;const router = express.Router();
+const userServer=require('./user_server')
+function getAllUsers(req, res) 
+{
+    userServer.getAllUsers(req, res)
+}
+function getById(req, res) 
+{
+    userServer.getById(req, res)
+}
+function putUser(req, res) 
+{
+    userServer.putUser(req, res)
+}
+function postUser(req, res) 
+{
+    userServer.postUser(req, res)
+}
+function deleteUser(req, res) 
+{
+    userServer.deleteUser(req, res)
+}
+module.exports = {getAllUsers,getById,putUser,postUser,deleteUser};
