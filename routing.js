d3acc36a1242ab64d0c2e521a3999dd0dac1c87e
MichalAmir <amichal9960@gmail.com>
2024-03-04 17:13:15 +0200

    delete this branch

---
+++ routing.js
@@ -0,0 +1,11 @@
+const express= require("express")
+const router =express.Router()
+const userController=require('../userController')
+
+router.get('/users', userController.gatAllUsers)
+router.get('/users/:id', userController.getById)
+router.put('/users/:id', userController.putUser)
+router.post('/users', userController.postUser)
+router.delete('/users/:id', userController.deleteUser)
+
+module.exports=router
