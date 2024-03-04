d2bf9b3d12498ad6e0ed4c74a452570d0c92f646
MichalAmir <amichal9960@gmail.com>
2024-03-04 18:03:33 +0200

    add space

--- routing.js
+++ routing.js
@@ -5,8 +5,13 @@ const router =express.Router()
 const userController=require('../controllers/userController')
 
 router.get('/users', userController.gatAllUsers)
+
 router.get('/users/:id', userController.getById)
+
 router.put('/users/:id', userController.putUser)
+
 router.post('/users', userController.postUser)
+
 router.delete('/users/:id', userController.deleteUser)
+
 module.exports=router
