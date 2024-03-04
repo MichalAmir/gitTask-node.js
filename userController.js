071745ef7fca5acf1cdfd244cc41b98d3efbf168
shulamitRoz <shulamit6670@gmail.com>
2024-03-04 16:39:05 +0200

    user-controller

--- userController.js
+++ userController.js
@@ -1,8 +1,6 @@
 const express = require("express")
 const app = express()
 const port = 8080
-
-// const userController= require('./controllers/userController')
  
 const users=[{ id:"1",name:"NameForExample",email:"example@gmail.com",phone:"0548559877"}]
 
