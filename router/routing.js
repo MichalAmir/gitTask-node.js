2f319d48faf3c177db5670621bd93eca835f76c6
AvigailH <avigail054855@gmail.com>
2024-03-07 16:55:24 +0200

    update routing

--- router/routing.js
+++ router/routing.js
@@ -4,13 +4,8 @@ const userController = require('../controllers/user.controller');
 
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getById);
-<<<<<<< HEAD
-router.put('/', userController.putUser);
-router.post('/:id', userController.postUser);
-=======
 router.put('/:id', userController.putUser);
 router.post('/', userController.postUser);
->>>>>>> a5327127425c39c7ab42259cdf7fdde5c1bc14ff
 router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
