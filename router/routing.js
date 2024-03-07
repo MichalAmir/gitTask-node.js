a27133370e9072a3226b768afcba2a781328471f
shulamitRoz <shulamit6670@gmail.com>
2024-03-07 16:48:09 +0200

    update-controller-services

--- router/routing.js
+++ router/routing.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/usercontroller');
+const userController = require('../controllers/user.controller');
 
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getById);
 router.put('/', userController.putUser);
-router.post('/', userController.postUser);
+router.post('/:id', userController.postUser);
 router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
